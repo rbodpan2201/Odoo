a13c6a7a40f5c7a3d9e7ae921e9516f3ee0d0d1e
rbodpan2201 <rbodpan2201@g.educaand.es>
2024-10-21 09:05:09 +0200

    Práctica 3. Vista.xml

--- pythonProject/ModuloNoNativo/inventarios/__manifest__.py
+++ pythonProject/ModuloNoNativo/inventarios/__manifest__.py
@@ -1,9 +1,9 @@
 {
     'name': 'Módulo inventarios',
     'version':'1.0',
-    'depends': ['base'],
+    'depends': ['base', 'contacts'],
     'author':'Raúl Bodas',
     'description': 'Módulo donde indicamos el inventario de Odoo',
-    'data': ['views/navmenu.xml'],
+    'data': ['views/navmenu.xml','views/vista.xml'],
     'installable': True,
 }
