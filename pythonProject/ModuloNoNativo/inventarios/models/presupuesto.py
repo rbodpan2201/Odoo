b1e4b17a87a7dd1145b4d797d9fbaa73450fdf62
rbodpan2201 <rbodpan2201@g.educaand.es>
2024-11-28 14:28:33 +0100

    Practica 7

--- pythonProject/ModuloNoNativo/inventarios/models/presupuesto.py
+++ pythonProject/ModuloNoNativo/inventarios/models/presupuesto.py
@@ -1,11 +1,21 @@
-from odoo import models, fields
+import logging
+from odoo import models, fields, api
+
+_logger = logging.getLogger("Raul Bodas")
 
 class Presupuesto(models.Model):
     _name="presupuesto"
     _inherit="image.mixin"
     name=fields.Char(string='Nombre', require=True)
     description = fields.Text(string='Descripción')
-    price = fields.Float(string='Precio', digit=(10,2))
+    unidades_producto = fields.Integer(string='Unidades del Producto', default=1)
+    precio_unitario = fields.Float(string='Precio Unitario', digits=(10, 2), default=0.0)
+    price = fields.Float(
+        string='Precio Total',
+        compute="_compute_price",
+        store=True,
+        digits=(10, 2),
+    )
     status = fields.Selection([
         ('draft' , 'Borrador'),
         ('confirmed' , 'Confirmado'),
@@ -27,9 +37,15 @@ class Presupuesto(models.Model):
         default=lambda self: self.env["res.partner.category"].search([('name', '=', 'ventas')], limit=1)
     )
 
+    @api.depends('unidades_producto', 'precio_unitario')
+    def _compute_price(self):
+        for record in self:
+            record.price = record.unidades_producto * record.precio_unitario
+
     def confirmar_presupuesto(self):
         if self.status == "draft":
             self.status="confirmed"
+            _logger.info(f"Presupuesto {self.name} confirmado")
 
 
     def terminar_presupuesto(self):
